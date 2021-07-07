987a7652adecedbf96ae87de2c362b46fdacee26
Mohammad Sartaj <sartajansari9786@gmail.com>
2021-07-07 21:09:13 +0530

    Added validation

--- train.py
+++ train.py
@@ -29,7 +29,6 @@ def train(args):
 		pass 
 	else:
 		raise TypeError("Enter Valid Model Name")
-	print(args["dataset_name"]+".pth")
 	if os.path.isfile(args["dataset_name"]+".pth"):
 		answer = input("Model is already exist would you like to load the weights (y/n): ")
 		if (answer.lower() == "y") or (answer.lower() == "yes"):
@@ -55,18 +54,31 @@ def train(args):
 	if int(args['classes']) == 2:
 		oneHot = False
 
-	dataset = Dataset(imageDir=args['images'], maskDir=args['masks'], 
+	trainDataset = Dataset(imageDir=args['train_images'], maskDir=args['train_masks'], 
 		imageSize=int(args['size']), oneHot=oneHot, numClasses=int(args['classes']))
 
-	dataloader = data.DataLoader(dataset, batch_size=args['batch_size'], shuffle=True, num_workers = 3)
+	dataloader = data.DataLoader(trainDataset, batch_size=args['batch_size'], shuffle=True, num_workers = 3)
+
+
+
+	if args['validation']:
+		testDataset = Dataset(imageDir=args['val_images'], maskDir=args['val_masks'], 
+			imageSize=int(args['size']), oneHot=oneHot, numClasses=int(args['classes']))
+
+		testDataLoader = data.DataLoader(testDataset, batch_size=args['batch_size'], shuffle=True, num_workers = 3)
+
+
 
 	for j in range(int(args['epochs'])):
 
 		torch.cuda.empty_cache()
+
+		model.train()
 		with tqdm(dataloader, unit="batch") as tepoch:
 			runningLoss = 0
 			torch.cuda.empty_cache()
 			for i, trainingData in enumerate(tepoch):
+
 				(image, mask) = trainingData
 				tepoch.set_description(f"Training Epoch {j + 1}")
 				image, mask = image.to(device), mask.to(device)
@@ -93,6 +105,39 @@ def train(args):
 					# tepoch.set_postfix(loss=runningLoss/len(dataloader), accuracy=100. * accuracy)
 					tepoch.set_postfix(loss=runningLoss/len(dataloader))
 
+
+
+		# Validation 
+		runningLoss = 0
+		model.eval()
+		if args['validation']:
+			with torch.no_grad():
+				with tqdm(testDataLoader, unit="batch") as tepoch:
+					torch.cuda.empty_cache()
+					for i, testingData in enumerate(tepoch):
+						(image, mask) = testingData
+						tepoch.set_description(f"Testing Epoch {j + 1}")
+						image, mask = image.to(device), mask.to(device)
+
+						output = model.forward(image)
+
+						loss = criterion(output, mask)
+
+						loss_value = loss.item()
+						runningLoss += loss_value
+
+						# tepoch.set_postfix(loss=loss.item(), accuracy=100. * train_acc)
+						tepoch.set_postfix(loss=loss_value)
+						time.sleep(0.005)
+
+						if i == len(testDataLoader) - 1:
+							# accuracy = accuracy / len(testDataLoader)
+							# tepoch.set_postfix(loss=runningLoss/len(testDataLoader), accuracy=100. * accuracy)
+							tepoch.set_postfix(loss=runningLoss/len(testDataLoader))
+
+
+
+
 	# torch.save(model, "final_model.pth")
 	torch.save(model.state_dict(), args["dataset_name"]+".pth")
 
@@ -103,8 +148,17 @@ if __name__ == "__main__":
 	args = vars(parser.parse_args())
 	with open(args['data'], "r") as f:
 		args = eval(f.read())
-	args['images'] = os.path.join(args['train_folder'], "images").replace("\\", "/")
-	args['masks'] = os.path.join(args['train_folder'], "masks").replace("\\", "/")
+	args['train_images'] = os.path.join(args['train_folder'], "images").replace("\\", "/")
+	args['train_masks'] = os.path.join(args['train_folder'], "masks").replace("\\", "/")
+
+	try:
+		if os.path.isdir(args["val_folder"]):
+			args['val_images'] = os.path.join(args['val_folder'], "images").replace("\\", "/")
+			args['val_masks'] = os.path.join(args['val_folder'], "masks").replace("\\", "/")
+
+			args['validation'] = True
+	except Exception as e:
+		args['validation'] = False
 
 	print(args)
 	train(args)
